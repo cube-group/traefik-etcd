5543093686092168d66519125bacde49782e73ff
linyang <7814560+LincolnBruce@users.noreply.github.com>
2018-11-23 20:14:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ sed -i "s;endpoint = \"127.0.0.1:2379\";endpoint = \"${ETCD_ENDPOINT}\";g" ${con
 #替换prefix
 sed -i "s;prefix = \"/traefik\";prefix = \"${ETCD_PREFIX}\";g" ${configFile}
 #替换etcd版本
-if [ "$ETCD_VERSION" == "v3" ];then
-    sed -i "s;#useAPIV3 = true;useAPIV3 = true;g" ${configFile}
+if [ "$ETCD_VERSION" == "v2" ];then
+    sed -i "s;useAPIV3 = true;#useAPIV3 = true;g" ${configFile}
 fi
 
 #supervisord -c /etc/traefik/supervisor.conf
