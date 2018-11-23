e379be62ca901e80509cf1b83c98dcbdcc0d5412
linyang <7814560+LincolnBruce@users.noreply.github.com>
2018-11-23 20:07:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,10 @@ configFile="/etc/traefik/traefik.toml"
 sed -i "s;endpoint = \"127.0.0.1:2379\";endpoint = \"${ETCD_ENDPOINT}\";g" ${configFile}
 #替换prefix
 sed -i "s;prefix = \"/traefik\";prefix = \"${ETCD_PREFIX}\";g" ${configFile}
+#替换etcd版本
+if [ "$ETCD_VERSION" == "v3" ];then
+    sed -i "s;#useAPIV3 = true;useAPIV3 = true;g" ${configFile}
+fi
 
-supervisord -c /etc/traefik/supervisor.conf
+#supervisord -c /etc/traefik/supervisor.conf
+traefik --configFile=/etc/traefik/traefik.toml
